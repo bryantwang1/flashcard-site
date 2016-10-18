6096bf6cf9368ee6c4d7c6bb66559c07168105b2
Bryant Wang <bryant.m.wang@gmail.com>
2016-10-18 14:38:11 -0700

    Add id's into index.html and respective toggles into scripts.js.

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+$(function() {
+  $("#vocab1").click(function() {
+    $("#hidden1").toggle();
+    $("#shown1").toggle();
+  });
+  $("#vocab2").click(function() {
+    $("#hidden2").toggle();
+    $("#shown2").toggle();
+  });
+  $("#vocab3").click(function() {
+    $("#hidden3").toggle();
+    $("#shown3").toggle();
+  });
+  $("#vocab4").click(function() {
+    $("#hidden4").toggle();
+    $("#shown4").toggle();
+  });
+  $("#vocab5").click(function() {
+    $("#hidden5").toggle();
+    $("#shown5").toggle();
+  });
+  $("#vocab6").click(function() {
+    $("#hidden6").toggle();
+    $("#shown6").toggle();
+  });
+  $("#vocab7").click(function() {
+    $("#hidden7").toggle();
+    $("#shown7").toggle();
+  });
+  $("#vocab8").click(function() {
+    $("#hidden8").toggle();
+    $("#shown8").toggle();
+  });
+  $("#vocab9").click(function() {
+    $("#hidden9").toggle();
+    $("#shown9").toggle();
+  });
+  $("#vocab10").click(function() {
+    $("#hidden10").toggle();
+    $("#shown10").toggle();
+  });
+  $("#vocab11").click(function() {
+    $("#hidden11").toggle();
+    $("#shown11").toggle();
+  });
+  $("#vocab12").click(function() {
+    $("#hidden12").toggle();
+    $("#shown12").toggle();
+  });
+  $("#vocab13").click(function() {
+    $("#hidden13").toggle();
+    $("#shown13").toggle();
+  });
+  $("#vocab14").click(function() {
+    $("#hidden14").toggle();
+    $("#shown14").toggle();
+  });
+  $("#vocab15").click(function() {
+    $("#hidden15").toggle();
+    $("#shown15").toggle();
+  });
+  $("#vocab16").click(function() {
+    $("#hidden16").toggle();
+    $("#shown16").toggle();
+  });
+  $("#vocab17").click(function() {
+    $("#hidden17").toggle();
+    $("#shown17").toggle();
+  });
+  $("#vocab18").click(function() {
+    $("#hidden18").toggle();
+    $("#shown18").toggle();
+  });
+  $("#vocab19").click(function() {
+    $("#hidden19").toggle();
+    $("#shown19").toggle();
+  });
+});
